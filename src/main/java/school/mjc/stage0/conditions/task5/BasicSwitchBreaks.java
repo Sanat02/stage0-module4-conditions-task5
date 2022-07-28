47cba027e0b2043adb6d9ea73bbee8094047c6a7
Sanat02 <86793142+Sanat02@users.noreply.github.com>
2022-07-28 17:51:02 +0600

    Update BasicSwitchBreaks.java

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitchBreaks.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitchBreaks.java
@@ -3,15 +3,22 @@ package school.mjc.stage0.conditions.task5;
 public class BasicSwitchBreaks {
     public static void main(String[] args) {
         int a = 20;
+         int a = 20;
         switch (a) {
-            default:
-                System.out.println("default");
-            case 7:
-                System.out.println("seven");
-            case 10:
-                System.out.println("ten");
             case 20:
                 System.out.println("twenty");
+                break;
+
+            case 7:
+                System.out.println("seven");
+                break;
+            case 10:
+                System.out.println("ten");
+                break;
+
+            default:
+                System.out.println("default");
+                break;
         }
     }
 }
