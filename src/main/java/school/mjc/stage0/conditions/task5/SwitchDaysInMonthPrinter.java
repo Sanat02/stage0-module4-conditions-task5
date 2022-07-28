55f7154ca1936fd663aee6759eea1661d6f0a26a
Sanat02 <86793142+Sanat02@users.noreply.github.com>
2022-07-28 18:16:57 +0600

    Update SwitchDaysInMonthPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,31 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        switch(month)
+     {
+         case 9:
+         case 4:
+         case 6:
+         case 11:
+
+             System.out.println("30");
+             break;
+         case 1:
+         case 2:
+         case 3:
+         case 5:
+         case 7:
+         case 8:
+         case 10:
+         case 12:
+
+             System.out.println("31");
+             break;
+         default:
+             System.out.println("wrong input!");
+             break;
+
+     }
+
     }
 }
